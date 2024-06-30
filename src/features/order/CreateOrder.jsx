22acf9422e5e755ae9b974f6ea1a59956db062d8
powiedl <159603185+powiedl@users.noreply.github.com>
2024-06-30 14:29:57 +0200

    added even more console.log

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -28,7 +28,7 @@ function CreateOrder() {
       </h2>
 
       {/* <Form method="POST" action="/order/new"> */}
-      <Form method="POST" action="/order/new">
+      <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
           <input
@@ -93,6 +93,7 @@ function CreateOrder() {
 }
 
 export async function action({ request }) {
+  console.log('CreateOrder action', request)
   const formData = await request.formData()
   const data = Object.fromEntries(formData)
   const order = {
