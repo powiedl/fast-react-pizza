c8d6d0e8a46c83f9accab06c1fdd802f3a1ff227
powiedl <159603185+powiedl@users.noreply.github.com>
2024-06-30 16:18:00 +0200

    CreateOrder-notworking is not working, CreateOrder does - can't spot the difference

--- src/features/order/CreateOrder-notworking.jsx
+++ src/features/order/CreateOrder-notworking.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { getCart } from '../cart/cartSlice'
 import { createOrder } from '../../services/apiRestaurant'
 import Button from '../../ui/Button'
-import EmptyCart from '../Cart/EmptyCart'
+import { useSelector } from 'react-redux'
+import { getCart } from '../cart/cartSlice'
+import EmptyCard from '../cart/EmptyCart'
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -13,32 +13,39 @@ const isValidPhone = (str) =>
   )
 
 function CreateOrder() {
+  // const [withPriority, setWithPriority] = useState(false);
   const navigation = useNavigation()
   const username = useSelector((state) => state.user.username)
   const isSubmitting = navigation.state === 'submitting'
-
   const formErrors = useActionData()
-
-  // const [withPriority, setWithPriority] = useState(false);
   const cart = useSelector(getCart)
-
-  if (!cart.length) return <EmptyCart />
+  if (!cart.length) return <EmptyCard />
 
   return (
     <div className="px-4 py-6">
-      <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
+      <h2 className="mb-8 text-xl font-semibold">
+        Ready to order? Let&apos;s go!
+      </h2>
 
       {/* <Form method="POST" action="/order/new"> */}
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow" type="text" name="customer" required />
+          <input
+            type="text"
+            name="customer"
+            required
+            className="input grow"
+            defaultValue={username}
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Phone number</label>
-          <div className="grow">
-            <input className="input w-full" type="tel" name="phone" required />
+          <div className="flex flex-col">
+            <div className="grow">
+              <input type="tel" name="phone" required className="input w-72" />
+            </div>
             {formErrors?.phone && (
               <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
                 {formErrors.phone}
@@ -51,32 +58,33 @@ function CreateOrder() {
           <label className="sm:basis-40">Address</label>
           <div className="grow">
             <input
-              className="input w-full"
               type="text"
               name="address"
               required
+              className="input w-full"
+              // className="w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-400 md:px-6 md:py-3"
             />
           </div>
         </div>
 
         <div className="mb-12 flex items-center gap-5">
           <input
-            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             type="checkbox"
             name="priority"
             id="priority"
+            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
           <label htmlFor="priority" className="font-medium">
-            Want to yo give your order priority?
+            Want you to give your order priority?
           </label>
         </div>
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button disabled={isSubmitting} type="primary">
-            {isSubmitting ? 'Placing order....' : 'Order now'}
+          <Button disabled={isSubmitting} type="primary" to="/menu">
+            {isSubmitting ? 'Placing order ...' : 'Order now'}
           </Button>
         </div>
       </Form>
@@ -87,25 +95,23 @@ function CreateOrder() {
 export async function action({ request }) {
   const formData = await request.formData()
   const data = Object.fromEntries(formData)
-
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
     priority: data.priority === 'on',
   }
+
   const errors = {}
   if (!isValidPhone(order.phone))
     errors.phone =
       'Please give us your correct phone number. We might need it to contact you.'
+  if (Object.keys(errors).length > 0) {
+    // ein Weg um festzustellen, ob ein Objekt zumindest ein Attribut hat
+    return errors
+  }
 
-  if (Object.keys(errors).length > 0) return errors
-
-  // If everything is okay, create new order and redirect
-
-  const newOrder = await createOrder(order)
-  return redirect(`/order/${newOrder.id}`)
-
-  //return null
+  // if everything is ok - no errors - create new order and redirect to that order
+  const newOrder = await createOrder(order) // newOrder ist das Objekt, dass von der API zurückkommt
+  return redirect(`/order/${newOrder.id}`) // daher hat es die id
 }
-
 export default CreateOrder
